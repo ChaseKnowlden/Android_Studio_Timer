0d6b560d95e559d0696a66e9d60c11b4501cec89
sihun <qorwlgml2003@naver.com>
2021-01-23 22:17:28 +0900

    Initial commit

---
+++ app/src/main/java/com/sihun/timer/MainActivity.java
@@ -0,0 +1,104 @@
+package com.sihun.timer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt_time, txt_breaktime, txt_round;
+    Button btn_start, btn_reset, open, btn_stop;
+    private Thread timeThread = null;
+    private Boolean isRunning = true;
+    int count = 0;
+    int min = 0;
+    int hour = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //xml파일의 id검색
+        txt_time = findViewById(R.id.txt_time);
+        txt_breaktime = findViewById(R.id.txt_breaktime);
+        txt_round = findViewById(R.id.txt_round);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_reset = findViewById(R.id.btn_reset);
+        btn_stop = findViewById(R.id.btn_stop);
+        open = findViewById(R.id.opendrawer);
+
+        //버튼 감지자 설정
+        btn_reset.setOnClickListener(click);
+        btn_start.setOnClickListener(click);
+        btn_stop.setOnClickListener(click);
+    }//oncreate
+
+    //버튼 클릭 감지자
+    View.OnClickListener click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_start:
+                        handler.sendEmptyMessage(0);
+                        btn_start.setVisibility(View.GONE);
+                        btn_stop.setVisibility(View.VISIBLE);
+                    break;
+
+                case R.id.btn_reset:
+                    handler.removeMessages(0);
+                    count = 0;
+                    int sec = 0;
+                    int min = 0;
+                    int hour = 0;
+                    String result = String.format("운동 \n%02d : %02d : %02d", hour, min, sec);
+                    txt_time.setText(result);
+                    btn_stop.setVisibility(View.GONE);
+                    btn_start.setVisibility(View.VISIBLE);
+                    break;
+
+                case R.id.btn_stop:
+                    handler.removeMessages(0);
+                    btn_stop.setVisibility(View.GONE);
+                    btn_start.setVisibility(View.VISIBLE);
+                    break;
+            }//switch
+        }
+    };//OnClickListener
+
+    Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage( Message msg) {
+            //1초에 한번씩 자신을 반복 수행
+            handler.sendEmptyMessageDelayed(0, 1000);
+
+            count++;
+
+            int sec = count;
+
+            if (count > 59){
+
+                count = 0;
+                min++;
+            };
+            if (min > 59){
+                count = 0;
+                min = 0;
+                hour++;
+            };
+
+            String result = String.format("운동 \n%02d : %02d : %02d", hour, min, sec);
+            txt_time.setText(result);
+        }
+    };
+
+
+
+}
